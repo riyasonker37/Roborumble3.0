2506a7c7877c79dc46de50d0730385688e706716
milliondreamsblog <akshatsan23@gmail.com>
2026-02-03 17:42:52 +0530

    updated hero

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen bg-[#01020a]">
       <HeroSection />
-      <HeroSectionNew />
+      {/* <HeroSectionNew /> */}
 
       <div className="relative z-20">
         <AboutUs />
